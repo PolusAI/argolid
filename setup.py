062d01d9be63b3d6a8a6c3e66adc10664dad545a
Vasu Jaganath <jaganathv2@nih.gov>
2025-11-18 10:57:29 -0500

    cleanup unnecessary changes for linux

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ class CMakeBuild(build_ext):
             result = subprocess.call(["sed", "-i", r"s/^#ifdef __has_builtin$/#if defined(__has_builtin) \&\& defined(__clang__)/", "_deps/tensorstore-src/tensorstore/internal/type_traits.h"],
                                      cwd=self.build_temp, env=env)
             if result != 0:
-                raise RuntimeError("TensorStore patch failed")
+                print("--------------- TensorStore patch failed!")
 
         subprocess.check_call(
             ["cmake", "--build", "."] + build_args, cwd=self.build_temp
